b2b9077060eeebd72d12fb6cd565ac370019bce6
natto1784 <singh.amneesh1784@gmail.com>
2021-06-12 14:03:59 +0530

    Added a post function

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 mod post;
 use actix_files::NamedFile;
 use actix_web::{web, App, HttpRequest, HttpServer};
-use std::{env, io, path::PathBuf};
+use std::{io, path::PathBuf};
 
 const ROOT_DIR: &str = "/files/";
 
-fn get_token() -> String {
-    env::var("FILEHOST_TOKEN").expect("FILEHOST_TOKEN is not set")
-}
-
-async fn get_file(req: HttpRequest) -> Result<NamedFile, io::Error> {
+async fn get_file(req: HttpRequest) -> Result<NamedFile,io::Error> {
     let file_name = req.match_info().get("file").unwrap_or("default");
     let file_path: PathBuf = format!("{}{}", ROOT_DIR, file_name).parse().unwrap();
     match NamedFile::open(file_path) {
@@ -28,7 +24,7 @@ async fn main() -> io::Result<()> {
             .route("/{file}", web::get().to(get_file))
             .route("/", web::post().to(post::post))
     })
-    .bind(("127.0.0.1", 8888))?
+    .bind(("0.0.0.0", 8888))?
     .run()
     .await
 }
--- src/post.rs
+++ src/post.rs
@@ -1,3 +1,61 @@
-pub fn post() -> &'static str {
-    "acha"
+use actix_multipart::Multipart;
+use futures::{StreamExt, TryStreamExt};
+use actix_web::{HttpRequest, HttpResponse};
+use async_std::prelude::*;
+use sanitize_filename::sanitize;
+use serde::Serialize;
+use std::{env, io};
+
+#[derive(Serialize)]
+struct ReturnData {
+    file: String,
+    host: String,
+    protocol: String,
+    url: String,
+}
+
+impl ReturnData {
+    fn new(_file:&str, _host: &str, _protocol: &str) -> Self {
+        Self {
+            file: String::from(_file),
+            host: String::from(_host),
+            protocol: String::from(_protocol),
+            url: format!("{}://{}/{}", _protocol, _host, _file),
+        }
+    }
+}
+
+const ROOT_DIR: &'static str = "/files/";
+
+fn get_token() -> String {
+    env::var("FILEHOST_KEY").expect("FILEHOST_KEY is not set")
+}
+
+pub async fn post(req: HttpRequest, mut payload: Multipart) -> Result<HttpResponse, io::Error> {
+    let key_header = req.headers().get("key");
+
+    if key_header.is_none() {
+        return Ok(HttpResponse::Unauthorized().body("No key provided"));
+    }
+
+    let key_val = key_header.unwrap().to_str().unwrap();
+
+    if key_val != get_token() {
+        return Ok(HttpResponse::Unauthorized().body("Invalid key provided"));
+    }
+
+    let mut filename = String::from("");
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        filename = sanitize(
+            field.content_disposition().unwrap().get_filename().unwrap(),
+        );
+        let filepath = format!("{}{}", ROOT_DIR, filename);
+        let mut f = async_std::fs::File::create(filepath).await?;
+
+        while let Some(chunk) = field.next().await {
+            let data = chunk.unwrap();
+            f.write_all(&data).await?;
+        }
+    }
+    Ok(HttpResponse::Ok().body(serde_json::to_string(&ReturnData::new(&filename, "f.weirdnatto.in", "https")).unwrap()))
 }
